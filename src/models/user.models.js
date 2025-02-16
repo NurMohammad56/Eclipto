d54c9490bb923b9813956078e6695c13819e1afe
NurMohammad56 <nurmohammad0605@gmail.com>
2025-02-16 22:25:35 +0600

    User Schema complete

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,14 +1,52 @@
 import mongoose, { Schema } from "mongoose";
 
-const userSchema = new Schema({
-  userName: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-    index: true,
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // Cloudinary url
+      unique: true,
+    },
+    coverImage: {
+      type: String, // Cloudinary url
+    },
+    watchHistory: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    password: {
+      type: String,
+      required: [true, "Password must be provided"],
+      minlength: 6,
+      select: false,
+    },
+    refreshToken: {
+      type: String,
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export const User = mongoose.model("User", userSchema);
