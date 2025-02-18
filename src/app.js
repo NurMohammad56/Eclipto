0937ea1e5a87d6e3d3345218cad996a0340a0c84
NurMohammad56 <nurmohammad0605@gmail.com>
2025-02-18 21:19:10 +0600

    feat(validation): validation complete for without coverImage and avatar

--- src/app.js
+++ src/app.js
@@ -12,8 +12,8 @@ app.use(
 );
 
 // Middleware to parse JSON and URL-encoded data
-app.use(express.json({ limit: "16k" }));
-app.use(express.urlencoded({ extended: true, limit: "16k" }));
+app.use(express.json({ limit: "100kb" }));
+app.use(express.urlencoded({ extended: true, limit: "100kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,33 @@
 import { AsyncHandler } from "../utils/AsyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
 
 const registerUser = AsyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "ok",
-  });
+  // get user details from frontend or postman
+  // validation - not empty
+  // check if user already exists : email, userName
+  // check for images and avatar
+  // upload them to cloudinary, then again check the avatar still exist or not
+  // create user object - create entry in db
+  // remove password and refresh token field from response
+  // check for user created successfully or not
+  // return success or error message
+
+  const { userName, email, fullName, password } = req.body;
+  // console.log("Email : ", email);
+
+  if (
+    [userName, email, fullName, password].some(
+      (field) => field?.trim("") === ""
+    )
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  if (!email.inclueds("@")) {
+    res.status(400).json({
+      message: "Invalid email format",
+    });
+  }
 });
 
 export { registerUser };
