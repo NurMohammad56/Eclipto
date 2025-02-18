b24baea0bc0a5448570d8588c6fd0f72f5838be1
NurMohammad56 <nurmohammad0605@gmail.com>
2025-02-18 12:01:02 +0600

    Implementation user routes and a basic controller.

--- src/app.js
+++ src/app.js
@@ -11,9 +11,16 @@ app.use(
   })
 );
 
+// Middleware to parse JSON and URL-encoded data
 app.use(express.json({ limit: "16k" }));
 app.use(express.urlencoded({ extended: true, limit: "16k" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// Import routes
+import { userRouter } from "./routes/user.routes.js";
+
+// Routes declaration
+app.use("/api/v1/users", userRouter);
 
 export { app };
