6220a12bf113ddb2fd9dcd5c55c3e378cf6ebae7
NurMohammad56 <nurmohammad0605@gmail.com>
2025-02-19 07:17:10 +0600

    check for images and avatar

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -13,9 +13,11 @@ const registerUser = AsyncHandler(async (req, res) => {
   // check for user created successfully or not
   // return success or error message
 
+  // get user details
   const { userName, email, fullName, password } = req.body;
   // console.log("Email : ", email);
 
+  // validation
   if (
     [userName, email, fullName, password].some(
       (field) => field?.trim("") === ""
@@ -24,18 +26,25 @@ const registerUser = AsyncHandler(async (req, res) => {
     throw new ApiError(400, "All fields are required");
   }
 
-  if (!email.inclueds("@")) {
-    res.status(400).json({
-      message: "Invalid email format",
-    });
+  // check email format
+  if (!email.includes("@")) {
+    throw new ApiError(400, "Invalid email format");
   }
 
-  const existedUser = User.findOne({
+  // check if user already exists
+  const existedUser = await User.findOne({
     $or: [{ email }, { userName }],
   });
 
   if (existedUser) {
-    throw new ApiError(400, "User already exists");
+    throw new ApiError(409, "User already exists");
+  }
+
+  // check for images and avatar
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  if (!avatarLocalPath || !coverImageLocalPath) {
+    throw new ApiError(400, "Avatar and cover image are required");
   }
 });
 
