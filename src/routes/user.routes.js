3db09a55fbbbcd1e3d6ef395ffbf72dddb6cc8e1
NurMohammad56 <nurmohammad0605@gmail.com>
2025-02-25 18:43:30 +0600

    feat(user): add routing for changing password, getting current user, updating profile, and retrieving user channel and watch history

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,9 +4,17 @@ import {
   registerUser,
   logOutUser,
   refreshAccessToken,
+  changePassword,
+  getCurrentUser,
+  updateUserProfile,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import upload from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "./../middlewares/auth.middleware.js";
+import upload from "./../middlewares/multer.middleware";
 
 const router = Router();
 
@@ -30,6 +38,32 @@ router.route("/login").post(loginUser);
 
 // logout
 router.route("/logout").get(verifyJWT, logOutUser);
+
 // refresh token
 router.route("/refresh-token").post(refreshAccessToken);
+
+// change passoword
+router.route("/change-password").post(verifyJWT, changePassword);
+
+// get user channel profile
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+
+// update user profile
+router.route("/update-account").patch(verifyJWT, updateUserProfile);
+
+// update user avatar
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+
+// update user cover image
+router
+  .route("/coverImage")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+
+// get user channel
+router.route("/channel/:userName").get(verifyJWT, getUserChannelProfile);
+
+// get watch historu
+router.route("/watchHistory").get(verifyJWT, getWatchHistory);
 export default router;
